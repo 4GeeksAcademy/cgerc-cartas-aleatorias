4d3047d4a6172234064940d2a5c8b45c8159a425
cgerc <gercanepacarmen@gmail.com>
2025-06-16 15:26:03 +0000

    generador casi listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
+  generadorCartasAleatorias();
+  document.querySelector("#nuevaCarta").addEventListener("click", generadorCartasAleatorias)
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generadorCartasAleatorias() {
+  let arrNumber = ['2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  let arrSymbol = ['♦', '♥', '♠', '♣'];
+
+
+  let numero = Math.floor(Math.random() * arrNumber.length);
+  let simbolo = Math.floor(Math.random() * arrSymbol.length);
+
+  let symbol = arrSymbol[simbolo];
+
+
+  document.querySelector(".top-left-icon").textContent = arrSymbol[simbolo];
+  document.querySelector(".top-right-icon").textContent = arrSymbol[simbolo];
+  document.querySelector(".center-number-icon").textContent = arrNumber[numero];
+  
+
+  const color = (symbol === '♦' || symbol === '♥') ? 'red' : 'black';
+  document.querySelector(".top-left-icon").style.color = color;
+  document.querySelector(".top-right-icon").style.color = color;
+  
+}
+
+
+
